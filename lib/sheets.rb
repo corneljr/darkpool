e50bf1e572972b7d70a2f6c8f54408aa7aa6167f
Josh Cornelius <joshcornelius@Joshs-MacBook-Air.local>
2016-10-25 11:13:58 -0400

    asddsa

--- lib/sheets.rb
+++ lib/sheets.rb
@@ -6,7 +6,7 @@ module Sheets
 		ws = session.spreadsheet_by_key("1boz24dpqMK0btRTL0XifIURNP0ae9WREm7LDzxrd2XU").worksheets[0]
 
 		travellers.each do |traveller|
-			sheet_hash = {"First Name" => traveller["firstName"], "Last Name" => traveller["lastName"], "Birthdate" => traveller["birthday"],"Gender" => traveller["gender"], "Email" => traveller["email"], "Origin" => origin, "Destination" => destination, "Departure Date" => departure_date, "Return Date" => return_date, "Tier" => tier, "Amount" => amount, "Booking ID" => booking_id}
+			sheet_hash = {"First Name" => traveller["firstName"], "Last Name" => traveller["lastName"], "Birthdate" => traveller["birthday"],"Gender" => traveller["gender"], "Email" => traveller["email"], "Origin" => origin, "Destination" => destination, "Departure Date" => departure_date, "Return Date" => return_date, "Tier" => tier, "Amount" => amount, "Booking ID" => booking_id, "Booking Date" => Time.now.strftime("%F")}
 			ws.list.push(sheet_hash)
 		end
 
