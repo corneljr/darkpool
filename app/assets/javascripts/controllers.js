e5be78e1986aca5390acfcc12c838deed1e5ec87
Josh Cornelius <joshcornelius@ip-96-49-101-10.lab.mtl>
2016-12-14 16:59:44 -0500

    asda

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -74,7 +74,7 @@ function ($scope,$rootScope,$window,$stateParams,$timeout,Flights, $ionicModal, 
 
     // override default behaviour
     $rootScope.$ionicGoBack = function() {
-        window.close();
+        window.location = "hopper-flights://home?tab=inbox"
 
         oldSoftBack();
     };
