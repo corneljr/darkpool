591d843a584bce56c00e48d9413ad60dd0cbed78
Josh Cornelius <joshcornelius@ip-96-49-101-10.lab.mtl>
2016-12-14 17:06:55 -0500

    mmm

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -74,7 +74,8 @@ function ($scope,$rootScope,$window,$stateParams,$timeout,Flights, $ionicModal, 
 
     // override default behaviour
     $rootScope.$ionicGoBack = function() {
-        window.location = "hopper-flights://home?tab=inbox"
+        // window.location = "hopper-flights://home?tab=inbox"
+        window.location = "https://www.hopper.com/linktest.html"
 
         oldSoftBack();
     };
